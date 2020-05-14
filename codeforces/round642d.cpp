1404fb1c7cb0b9084c2daabd9337c7841a748ba5
Gummybearr <ehrudghl123@naver.com>
2020-05-15 04:04:17 +0900

    Update round642d.cpp after being hacked

--- codeforces/round642d.cpp
+++ codeforces/round642d.cpp
@@ -1,35 +1,26 @@
 #include <bits/stdc++.h>
-using namespace std;
 #pragma warning(disable: 4996)
-#define pii pair<int, int>
+using namespace std;
+#define ll long long
 
 int t, n;
-int num[1 << 20];
-
-struct cmp {
-	bool operator()(pair<int, pii> a, pair<int, pii> b) {
-		return a.first == b.first ? a.second.first>b.second.first : a.first<b.first;
-	}
-};
+int num[1<<20];
+priority_queue <pair<int, pair<int, int>>> q;
 
 int main() {
-	scanf("%d ", &t);
+	scanf("%d", &t);
 	while (t--) {
-		scanf("%d", &n);
-		fill(num, num + (1 << 20), 0);
-		priority_queue<pair<int, pii>, vector<pair<int, pii>>, cmp> pq;
-		pq.push({ n, {0,n - 1} });
-		for(int i = 0;i<n;i++) {
-			if (pq.empty()) break;
-			auto it = pq.top();
-			int l = it.second.first, r = it.second.second;
-			pq.pop();
+		cin >> n;
+		q.push({ n,{-1,n} });
+		for (int i = 1; i <= n; i++) {
+			auto it = q.top();
+			q.pop();
+			int l = -it.second.first, r = it.second.second;
 			int tval = (l + r) >> 1;
-			num[tval] = i + 1;
-			if(l<=tval-1)	pq.push({ tval - l+1, {l,tval - 1} });
-			if (r>=tval + 1)	pq.push({ r - tval+1, {tval + 1, r} });
+			num[tval] = i;
+			if (tval - 1 - l + 1 > 0) q.push({ tval - 1 - l + 1,{-l,tval - 1} });
+			if (r - tval > 0) q.push({ r - tval,{-tval - 1,r} });
 		}
-		for (int i = 0; i < n; i++) { printf("%d ", num[i]); }	printf("\n");
+		for (int i = 1; i <= n; i++) printf("%d ", num[i]);	printf("\n");
 	}
-	return 0;
 }
